a8e2c8fc6ff2087bce533f61c18c142458c57434
Hendi Santika <hendisantika@yahoo.co.id>
2019-02-04 17:40:51 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootpid/SpringBootPidApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootpid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPidApplication.class, args);
+    }
+
+}
+
