a81251224604cb6c4841bba232c32fdda8ce5012
Hendi Santika <hendisantika@yahoo.co.id>
2019-02-04 17:43:58 +0700

    #3 - Modify Main Class

--- src/main/java/com/hendisantika/springbootpid/SpringBootPidApplication.java
+++ src/main/java/com/hendisantika/springbootpid/SpringBootPidApplication.java
@@ -2,12 +2,17 @@ package com.hendisantika.springbootpid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class SpringBootPidApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootPidApplication.class, args);
+//        SpringApplication.run(SpringBootPidApplication.class, args);
+        SpringApplication springApplication = new SpringApplication(SpringBootPidApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
+
     }
 
 }
